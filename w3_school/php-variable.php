b790b037bc51bb426616c99228fac745c458cefb
f0anits1o <avotrinainafidelis@gmail.com>
2023-09-29 17:52:06 +0300

    php variable

---
+++ w3_school/php-variable.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $txt = "Hello world!";
+    $x = 5;
+    $y = 10.5;
+
+    echo $txt;
+    echo "<br>";
+    echo $x;
+    echo "<br>";
+    echo $y;
+    echo "<br>";
+    ?>
+
+    <?php
+    $txt = "Micheline";
+    echo "I love $txt!";
+    echo "<br>";
+    ?>
+
+    <?php
+    $txt = "W3Schools.com";
+    echo "I love " . $txt . "!";
+    echo "<br>";
+    ?>
+
+    <?php
+    $x = 5;
+    $y = 4;
+    echo $x + $y;
+    ?>
+
+
+</body>
+</html>

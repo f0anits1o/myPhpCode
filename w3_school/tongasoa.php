909874dd8c3d752fc8cb07be9f15a97e2269dbf5
f0anits1o <avotrinainafidelis@gmail.com>
2023-09-29 16:17:36 +0300

    add 3 files '

---
+++ w3_school/tongasoa.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo ' Salama <br>';
+        echo " Date is " .date("d-m-y, h:i:s");
+    ?>
+    
+</body>
+</html>
